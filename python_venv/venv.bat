07a9c594a1703dc748eff8de4f008ce863dbbd4e
Danielle Rodriguez <danielleskyerodriguez@gmal.com>
2024-09-07 12:38:14 -0500

    python version validation

--- python_venv/venv.bat
+++ python_venv/venv.bat
@@ -1,4 +1,4 @@
-@echo off 
+@echo off
 setlocal enabledelayedexpansion
 
 @REM working directory is python_venv
@@ -7,9 +7,9 @@ set venv=%workingDir%\venv
 set venvRequirements=%workingDir%\requirements.txt
 
 @REM find python alias for system
-( 
+(
     call :check_system_alias python3 && set pythonCmd=python3 
-) || ( 
+) || (
     call :check_system_alias python && set pythonCmd=python 
 ) || (
     call :check_system_alias py && set pythonCmd=py
@@ -17,38 +17,48 @@ set venvRequirements=%workingDir%\requirements.txt
     echo error could not find python.exe in system path && goto :eof
 )
 
-@REM create virtual environment
-if exist %venv% (
-    echo cleaning up environment
-    ( call :deactivate_venv && rmdir /q /s %venv% ) || (
-        echo error cleaning up virtual environment && goto :eof 
+@REM check python version supports virtual environments
+set minimumPythonVersion=Python 3.5
+for /f "delims=" %%v in ('%pythonCmd% --version') do set pythonVersion=%%v
+if not defined pythonVersion (
+    echo error detecting pythonVersion && goto :eof
+)
+for /f "tokens=2,3,4 delims=. " %%a in ("%minimumPythonVersion%") do (
+    for /f "tokens=2,3,4 delims=. " %%i in ('%pythonCmd% --version') do (
+        if %%i lss %%a (
+            echo error upgrade python to version %minimumPythonVersion% && exit /b 1
+        )
+        if %%j lss %%b (
+            echo error upgrade python to version %minimumPythonVersion% && exit /b 1
+        )
     )
 )
+echo using %pythonVersion%
 
-( %pythonCmd% -m venv %venv% && echo virtual environment created ) || (
-    echo error creating virtual environment && goto :eof 
+@REM create virtual environment
+( %pythonCmd% -m venv --clear %venv% && echo virtual environment created ) || (
+    echo error creating virtual environment && goto :eof
 )
 
 @REM find alias for the virtual environment
-( 
-    call :check_venv_alias python3 && set pythonCmd=python3 
-) || ( 
-    call :check_venv_alias python && set pythonCmd=python 
+(
+    call :check_venv_alias python3 && set pythonCmd=python3
+) || (
+    call :check_venv_alias python && set pythonCmd=python
 ) || (
     call :check_venv_alias py && set pythonCmd=py
 ) || (
     echo error could not find python.exe in virtual environment, exiting  && goto :eof
 )
 
-@REM activate virtual environment and install packages
 ( call :activate_venv ) || ( echo exiting %errorlevel% && goto :eof )
 
 echo starting package installation
-( %pythonCmd% -m pip install -r %venvRequirements% > nul ) || ( 
+( %pythonCmd% -m pip install -r %venvRequirements% > nul ) || (
     echo error installing packages && call :deactivate_venv && goto :eof
 )
-
-echo virtual environment ready && call :deactivate_venv
+%pythonCmd% -m pip list
+echo virtual environment ready
 goto :eof
 
 :activate_venv
@@ -88,12 +98,6 @@ goto :eof
         ) 
     )
     set /A %~1=0 && exit /b 0
-
-:check_version
-    @REM TODO: check version
-    @REM for /f "delims=" %%i in ('%pythonAlias% --version') do ( set pythonVersion=%%i )
-    @REM echo %pythonCmd% using version %pythonVersion%
-    exit /b 0 
 
 :check_system_alias 
     where %~1 2> nul 1> nul && exit /b 0 || exit /b 1  
